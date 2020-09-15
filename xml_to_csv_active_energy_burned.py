718ec96c6b4f8c49f9c1564192c5a411b40e1fab
Art K <beijingbayou@gmail.com>
2020-09-14 19:28:29 -0700

    added apple_health_xml_to_csv.py; updated  xml_to_csv_active_energy_burned.py to use AppleHealthActiveEnergyBurnedXmlCsv class.

--- xml_to_csv_active_energy_burned.py
+++ xml_to_csv_active_energy_burned.py
@@ -4,12 +4,22 @@ from healthdata import HK_REC_TYPE_ActiveEnergyBurned
 from xml_to_csv_argparser import parse_cmdline
 from xml_to_csv_record import  xml_to_csv
 
+from apple_health_xml_to_csv import AppleHealthActiveEnergyBurnedXmlCsv
 
 if __name__ == '__main__':
     args = parse_cmdline(prog=pathlib.PurePath(__file__).name,
                          description="Extracts VO2 max history from Apple Health Data xml file.")
-
+    """
     xml_to_csv(args.xml_filepath, args.csv_filepath,
                args.start_date, args.end_date,
                args.sort, args.watch_only_data,
                HK_REC_TYPE_ActiveEnergyBurned)
+    """
+
+    csv_loader = AppleHealthActiveEnergyBurnedXmlCsv(
+        args.xml_filepath, args.csv_filepath,
+        args.start_date, args.end_date,
+        args.sort, args.watch_only_data
+    )
+
+    csv_loader.serialize()
