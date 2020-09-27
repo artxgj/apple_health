19344fac071c950cb594a48af40137962ad9187b
Art K <beijingbayou@gmail.com>
2020-09-27 16:30:33 -0700

    90% done: blank-weighin-intervals-activity-metrics.ipynb

--- first_weighin_interval_activity_mapping.py
+++ first_weighin_interval_activity_mapping.py
@@ -1,6 +1,6 @@
+import argparse
 import csv
 import pathlib
-from typing import Any, Dict, Iterator, Tuple
 
 from intervals import month_firstdate_intervals, map_elements_to_intervals
 from utils import weighin_date_group_key
@@ -9,6 +9,10 @@ from utils import weighin_date_group_key
 def tocsv_activity_interval_map(weights_csvpath: str,
                                 activity_summary_csvpath: str,
                                 activity_summary_dates_intervals_csvpath: str):
+    """Map activity summary dates to weigh-in intervals
+    This provides a way to calculate total calories, total exercise time, etc. between
+    weigh-in periods that are at least a month-apart.
+    """
     with open(weights_csvpath, "r", encoding="utf-8") as f1, \
             open(activity_summary_csvpath, "r", encoding="utf-8") as f2, \
             open(activity_summary_dates_intervals_csvpath, "w", encoding="utf-8") as f3:
@@ -31,8 +35,14 @@ def tocsv_activity_interval_map(weights_csvpath: str,
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog=__file__,
+                                     description='Generates summary datasets of cumulative quantity sample types.')
+
+    parser.add_argument('-partition-date', type=str, required=True, help='partition-date subfolder')
+    args = parser.parse_args()
+
     home = pathlib.Path.home()
-    partition_date = "20200925"
+    partition_date = args.partition_date
     health_csv_folder = f"{home}/small-data/apple-health-csv/full-extract"
     weights_csvpath = f"{health_csv_folder}/{partition_date}/bodymass-summary.csv"
     activity_summary_csvpath = f"{health_csv_folder}/{partition_date}/activity-summary.csv"
